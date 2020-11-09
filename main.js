3f94a675cef62dd0fe2b8f8175f2e6cb3b260403
Alba De biase <debiasealba@gmail.com>
2020-11-09 15:40:23 +0100

    struttura

---
+++ main.js
@@ -0,0 +1,11 @@
+// Creare un carousel di immagini con Vue!
+// bonus: aggiungere anche l'autoplay con una timing functions.
+// bonus2:aggiungete come secondo bonus anche il click sui pallini per cambiare immagine!
+
+
+var app = new Vue({
+  el: '#container',
+  data: {
+    immagini: 'https://www.frasidadedicare.it/frasidadedicare/wp-content/uploads/2020/07/immagini-belle-con-frasi-significative-2.jpg!'
+  }
+})
